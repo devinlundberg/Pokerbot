3ca5ccca76f6784c5d37ce017c70cf48d02fc90c
Robert Nubel <robert.nubel@gmail.com>
2012-02-17 00:28:29 -0600

    Adds a dummy seating controller that always accepts.

---
+++ app/controllers/seating.rb
@@ -0,0 +1,25 @@
+SamplePokerBot.controllers :seating do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  post "/" do
+    status 200
+    "I accept!"
+  end
+end
